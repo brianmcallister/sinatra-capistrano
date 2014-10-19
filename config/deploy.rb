0713d6646547d5d04b41df37ea5802c9615977f3
Brian McAllister <brian@brianmcallister.com>
2014-10-19 12:48:05 -0400

    Echo out some paths.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,17 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:web) do      
+      within deploy_path do
+        execute :echo, 'deploy_path ----------------'
+        execute :pwd
+      end
+      
+      within release_path do
+        execute :echo, 'release_path ----------------'
+        execute :pwd
+      end
+      
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
     end
