69e35846c14e1d40451912031e34dca0d1879a08
Brian McAllister <brian@brianmcallister.com>
2014-10-21 19:45:22 -0400

    Set up some capistrano plugins.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source "https://rubygems.org"
 
 gem 'sinatra', '~> 1.4.5'
-gem 'capistrano', '~> 3.2.1'
+
+group :development do
+  gem 'capistrano', '~> 3.2.1'
+  gem 'capistrano-bundler', '~> 1.1.3'
+  gem 'capistrano-rbenv', '~> 2.0.2'
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,12 @@ set :application, 'sinatra-capistrano'
 set :repo_url, 'git@github.com:brianmcallister/sinatra-capistrano.git'
 set :branch, 'master'
 
+# rbenv setup.
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.2'
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
