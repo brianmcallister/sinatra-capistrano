1bdeb9331b87f076fc6df68dd31d7297a10d9525
Brian McAllister <brian@brianmcallister.com>
2014-10-22 19:08:00 -0400

    Get binaries working with rbenv correctly.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,11 @@ set :branch, 'master'
 # capistrano/rbenv setup.
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sass}
 
 # capistrano/bundler setup.
 set :bundle_bins, fetch(:bundle_bins, []).push('sass')
-
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
